ab997995e6066ff3c8f84c22b1108d63c039dea3
Лысов Глеб <lysovg9@gmail.com>
2025-06-08 12:00:17 +0300

    feat(nvim): moved to lazy-vim

---
+++ nvim/lua/plugins/term.lua
@@ -0,0 +1,16 @@
+return {
+  {"akinsho/toggleterm.nvim",
+	config = function()
+		require"toggleterm".setup {
+			open_mapping = [[<c-\>]]
+		}
+
+		local function split_terminal_right()
+		  local Terminal = require('toggleterm.terminal').Terminal
+		  Terminal:new({direction='horizontal'}):open()
+		end
+
+		vim.api.nvim_create_user_command('SplitTerminal', split_terminal_right, {})
+		vim.keymap.set({"t"}, "<c-s>", "<cmd>SplitTerminal<cr>")
+	end},
+}
---
+++ nvim/lua/plugins/ui/bufferline.lua
@@ -0,0 +1,75 @@
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+signs = {
+	error = signs.Error,
+	warning = signs.Warn,
+	info = signs.Info,
+	hint = signs.Hint,
+}
+
+local severities = {
+	"error",
+	"warning",
+	-- "info",
+	-- "hint",
+}
+
+return {
+  {
+    "akinsho/bufferline.nvim",
+    tag = "v4.5.2",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+      { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+      { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+    },
+    opts = {
+      options = {
+        -- stylua: ignore
+        close_command = function(n) Snacks.bufdelete(n) end,
+        -- stylua: ignore
+        right_mouse_command = function(n) Snacks.bufdelete(n) end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+		diagnostics_indicator = function(_, _, diag)
+			local s = {}
+			for _, severity in ipairs(severities) do
+				if diag[severity] then
+					table.insert(s, signs[severity] .. diag[severity])
+				end
+			end
+			return table.concat(s, " ")
+		end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+          {
+            filetype = "snacks_layout_box",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
+}
